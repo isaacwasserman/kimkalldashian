4b76d965bac8cac0c2c0c67e3fb53bf70ddc0ef2
Isaac Wasserman <iwasserblaster@gmail.com>
2014-12-30 14:03:14 -0500

    Twilio

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var twilio = require("../node_modules/twilio");
 var bodyParser = require('body-parser');
 
 
@@ -17,6 +17,21 @@ app.post('/submit', function(req, res) {
   console.log(req.body);
   res.redirect('/');
 });
+
+
+// Your accountSid and authToken from twilio.com/user/account
+var accountSid = 'ACc35629f131fe39cc5b112a761d7492c0';
+var authToken = "ae348584f7264cd2ff0d741c05b2bb32";
+var client = require('twilio')(accountSid, authToken);
+ 
+client.messages.create({
+    body: "Hello Isaac",
+    to: "+18563322539",
+    from: "+14243552041"
+}, function(err, message) {
+    process.stdout.write(message.sid);
+});
+
 
 app.get('/', function(request, response) {
   // send mail with defined transport object
