99f0b4a28b62259d2d25abe8ab60d5ff638787a8
Isaac Wasserman <iwasserblaster@gmail.com>
2015-01-05 18:35:27 -0500

    error

--- routes/subscription.js
+++ routes/subscription.js
@@ -13,26 +13,4 @@ router.get('/', function(req, res) {
   });
 });
 
-router.post('/', function(req, res) {
-  
-  console.log('Input Submitted');
-  console.log("Input Value: " + req.body.number);
-  
-  var input = req.body.number;
-  
-  if(input.match(/^[0-9]+$/) != null && input.length >= 10){
-    var subscriber = new Subscription({number: req.body.number, twitter: 'kimkardashian'});
-    subscriber.save(function (err, subscriber, numberAffected) {
-      if (err) { 
-        console.log(err); 
-      }
-    });
-  } else {
-    console.log("Input is not correctly formatted");
-  }
-  
-  res.redirect('/');
-  
-});
-
 module.exports = router;
